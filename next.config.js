822f15cfaa8e299801ef191d9b8dffc6d661b53e
Pawan0603 <www.pawanmthakre@gmail.com>
2024-03-27 21:16:34 +0530

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    basePath: '/github-pages',
+    // basePath: '/github-pages',
     images: {
         domains: ['res.cloudinary.com'],
     },
