599e634ed18ff2fb9ce8868a53f7493748ff5149
Pawan0603 <www.pawanmthakre@gmail.com>
2024-03-28 21:08:10 +0530

    modified: next.config.js & home page image src

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
-    // output: 'export',
+    output: 'export',
+    distDir: 'dist',
 }
 
 module.exports = nextConfig
